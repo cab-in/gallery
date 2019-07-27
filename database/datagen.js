7172590572ff8da042af65d57a2c41f46df26d68
Andrew Poon <akpoon@gmail.com>
2019-07-27 01:08:16 -0700

    wrapped in promise and write to csv

--- database/datagen.js
+++ database/datagen.js
@@ -1,30 +1,47 @@
 const fs = require('fs');
 const faker = require('faker');
 
+const fileStream = fs.createWriteStream('./gallery.csv');
+
+function write() {
+  return new Promise((resolve, reject) => {
+    fileStream.once('drain', resolve);
+    fileStream.on('error', reject);
+  });
+}
+
 // insert the string 'house' or 'interior'
 const imageGen = (imageType) => {
-  const imageRandom = Math.floor(Math.random() * 1000) + 1;
+  const imageRandom = Math.floor(Math.random() * 500) + 1;
   const str = `${imageRandom}`;
-  const pad = '0000';
+  const pad = '000';
   const imageNumber = pad.substring(0, pad.length - str.length) + str;
   return `https://cabin-gallery.s3-us-west-1.amazonaws.com/${imageType}-${imageNumber}.jpg`;
 };
-const images = [];
 
-for (let i = 0; i < 50; i += 1) {
-  for (let j = 0; j < 6; j += 1) {
-    const image = [];
-    const listingid = i + 1;
-    let url = '';
-    if (j === 0) {
-      url = imageGen('house');
-    } else {
-      url = imageGen('interior');
+async function writer() {
+  let ableToWrite = true;
+  for (let i = 0; i < 2; i += 1) {
+    for (let j = 0; j < 6; j += 1) {
+      const listingid = i + 1;
+      let url = '';
+      // only first image of a listing has house image, the rest are interior
+      if (j === 0) {
+        url = imageGen('house');
+      } else {
+        url = imageGen('interior');
+      }
+      const caption = faker.lorem.words();
+      const verified = Math.round(Math.random());
+
+      ableToWrite = fileStream.write(`${listingid},${url},${caption},${verified}\n`);
+
+      if (!ableToWrite) {
+        await write();
+      }
     }
-    const caption = faker.lorem.words();
-    const verified = Math.round(Math.random());
-    image.push(listingid, url, caption, verified);
-    images.push(image);
   }
+  fileStream.end();
 }
-console.log(images);
+
+writer();
