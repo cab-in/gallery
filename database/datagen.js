a905114233b796bf6a5fa0f95c55323b5fbb03cf
Andrew Poon <akpoon@gmail.com>
2019-07-27 00:21:15 -0700

    adding basic gen components, no csv yet

--- database/datagen.js
+++ database/datagen.js
@@ -1,14 +1,29 @@
 const fs = require('fs');
 const faker = require('faker');
 
+// insert the string 'house' or 'interior'
+const imageGen = (imageType) => {
+  const imageRandom = Math.floor(Math.random() * 1000) + 1;
+  const str = `${imageRandom}`;
+  const pad = '0000';
+  const imageNumber = pad.substring(0, pad.length - str.length) + str;
+  return `https://cabin-gallery.s3-us-west-1.amazonaws.com/${imageType}-${imageNumber}.jpg`;
+};
+const images = [];
 
-const imageRandom = Math.floor(Math.random() * 1000) + 1;
-const str = `${imageRandom}`;
-const pad = '0000';
-const imageNumber = pad.substring(0, pad.length - str.length) + str;
-const imageURL = `https://cabin-gallery.s3-us-west-1.amazonaws.com/house-${imageNumber}.jpg`;
+for (let i = 0; i < 100; i++) {
+  for (let j = 0; j < 6; j++) {
+    const image = [];
+    const listingid = i + 1;
+    const url = imageGen();
+    const caption = faker.lorem.words();
+    const verified = Math.round(Math.random());
+    image.push(listingid, url, caption, verified);
+    images.push(image);
+  }
+}
+console.log(images);
 
-console.log(imageURL);
 
-image.push(listingid, url, caption, verified);
-images.push(image);
+// image.push(listingid, url, caption, verified); //each image
+// images.push(image); // array of all images
